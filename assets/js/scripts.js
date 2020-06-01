9d319f89299b6b6bb4912e3e1daa08729734c8ec
Quentin Brouillet <quent07@hotmail.com>
2020-06-01 11:34:14 +0200

    add masonry and fancybox library

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+// document.getElementById('enter').addEventListener('click', (e) => {
+//     console.log('hello');
+
+// });
+
+$(document).ready(function () {
+    $('.scrollTo').on('click', function () { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        console.log(page);
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate({
+            scrollTop: $(page).offset().top
+        }, speed); // Go
+        return false;
+    });
+});
+
+// Masonry
+
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
