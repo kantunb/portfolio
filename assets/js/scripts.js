d9dd0c0dff078d6b814a19ea2de53ec67aa9792f
KantunB <quentin.brouillet+git@gmail.com>
2020-06-03 23:11:26 +0200

    masonry and fancybox integration

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,24 @@
-// document.getElementById('enter').addEventListener('click', (e) => {
-//     console.log('hello');
+window.onload = function () {
 
-// });
-
-$(document).ready(function () {
-    $('.scrollTo').on('click', function () { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        console.log(page);
-        var speed = 750; // Durée de l'animation (en ms)
-        $('html, body').animate({
-            scrollTop: $(page).offset().top
-        }, speed); // Go
-        return false;
+    $(document).ready(function () {
+        $('.scrollTo').on('click', function () { // Au clic sur un élément
+            var page = $(this).attr('href'); // Page cible
+            console.log(page);
+            var speed = 750; // Durée de l'animation (en ms)
+            $('html, body').animate({
+                scrollTop: $(page).offset().top
+            }, speed); // Go
+            return false;
+        });
     });
-});
 
-// Masonry
+    // Masonry
 
-$('.grid').masonry({
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+
+};
